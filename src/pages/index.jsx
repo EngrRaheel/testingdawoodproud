f96c3de7910eb860f60616bb1c0c94681cfb6ca0
Engr. Raheel <engr.mraheeltariq@gmail.com>
2023-07-10 17:33:13 +0500

    ok done

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,7 @@ import ServiceCards from "../../components/Services components/ServiceCards"
 
 import SuperchargeYT from "../../components/ConsultationForm/CallToAction";
 import YtSlider from "../../components/YtSlider/Slider";
-
+import Head from "next/head";
 import Footer from "../../components/common/Footer";
 
 
@@ -13,7 +13,26 @@ import Footer from "../../components/common/Footer";
 const Home = () => {
   return (
     <div className=" bg-[white] ">
-      {/* [url('/Images/main-bg1.jpg')] */}
+      <Head>
+        <title>
+        Dawood Proud: YouTube Agency for all YouTube Growth Services
+        </title>
+        <meta
+          name="description"
+          content="Leading YouTube Channel Growth Agency that offers comprehensive Content Strategy and Best YouTube Growth Services to dominate the platform."
+        />
+        <meta
+          name="keywords"
+          content=" YouTube Agency, YouTube Growth Agency, YouTube Channel Growth Agency, Dawood Proud, DawoodProud"
+        />
+        <meta property="og:title" content="YouTube Consultation" />
+        <meta
+          property="og:description"
+          content="This is the YouTube Consultation page description."
+        />
+        <meta property="og:image" content="" />
+      </Head>
+
       <Hero />
 
       <YtSlider />
